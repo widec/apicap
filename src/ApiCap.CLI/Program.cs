f7655333bbab6a35fea11c4adb42cf5edb989673
Wim De Cleen <wim>
2017-11-01 10:37:08 +0100

    MsBuild task to capture api

--- src/ApiCap.CLI/Program.cs
+++ src/ApiCap.CLI/Program.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApiCap
+namespace ApiCap.CLI
 {
     class Program
     {
---
+++ src/ApiCap.MsBuild/ApiCapture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ApiCap.MsBuild
+{
+    public class ApiCapture : Task
+    {
+        public override bool Execute()
+        {
+            var assemblies = Assemblies.Select(item => Assembly.LoadFrom(item.ItemSpec)).ToArray();
+            var outputPath = OutputFileName == null ? "output.txt" : OutputFileName.ItemSpec;
+            var visitor = new FileAssemblyVisitor(outputPath);
+
+            visitor.Visit(assemblies);
+            return true;
+        }
+
+        public ITaskItem[] Assemblies { get; set; }
+        public ITaskItem OutputFileName { get; set; }
+
+    }
+}
